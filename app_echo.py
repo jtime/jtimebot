9ce695658e5fe4fad44826751f15973e84d8a56e
jtime <sgr918@gmail.com>
2021-12-20 23:11:08 +0800

    echo

--- app_echo.py
+++ app_echo.py
@@ -26,11 +26,11 @@ def callback():
 
 @handler.add(MessageEvent, message = TextMessage)
 def echo(event):
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 if __mane__ == "__main__":
     app.run()
