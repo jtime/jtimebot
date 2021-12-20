8ab1df56bec8595bad470c085299fb340db69e24
jtime <sgr918@gmail.com>
2021-12-20 23:51:30 +0800

    21/12/20 2351 echo

--- app_echo.py
+++ app_echo.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 import configparser
 
 app = Flask(__name__)
