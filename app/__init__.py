4f9a484d3840b3d12a16d25e83aeb1d6dd6581d9
jtime <sgr918@gmail.com>
2022-01-02 00:20:35 +0800

    4-22 test2

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from linebot import LineBotApi, WebhookHandler
+import configparser
+
+app = Flask(__name__)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+from app import routes, models_for_line
