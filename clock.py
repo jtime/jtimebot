cf4686ee057e070c139fb9b7d61a1ab5a02aca41
jtime <sgr918@gmail.com>
2022-01-13 01:01:28 +0800

    202201130101

--- clock.py
+++ clock.py
@@ -1,30 +1,20 @@
-# import datetime
-# from apscheduler.schedulers.blocking import BlockingScheduler
-# import urllib.request
-#
-# sched = BlockingScheduler()
-#
-#
-# @sched.scheduled_job('cron', minute='*/2')
-# def scheduled_job():
-#     print('this job runs every day */2 min.')
-#     print(f"{datetime.datetime.now().ctime()}")
-#     print('====== APScheduler CRON ==========')
-#
-#     url = "https://jtimebot.heroku.com/"
-#     conn =urllib.request.urlopen(url)
-#
-#     for key,value in conn.getheaders():
-#         print(key, value)
-#
-# sched.start()
-
+import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
+import urllib.request
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+
+@sched.scheduled_job('cron', minute='*/2')
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print('this job runs every day */2 min.')
+    print(f"{datetime.datetime.now().ctime()}")
+    print('====== APScheduler CRON ==========')
+
+    url = "https://jtimebot.heroku.com/"
+    conn =urllib.request.urlopen(url)
+
+    for key,value in conn.getheaders():
+        print(key, value)
 
 sched.start()
