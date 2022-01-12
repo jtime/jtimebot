4f85f64165247c0bdc5743e43daec6d815369952
jtime <sgr918@gmail.com>
2022-01-12 23:31:31 +0800

    20220112/apscheduler test

--- clock.py
+++ clock.py
@@ -1,5 +1,4 @@
 import datetime
-
 from apscheduler.schedulers.blocking import BlockingScheduler
 import urllib.request
 
@@ -9,16 +8,16 @@ sched = BlockingScheduler()
 # def timed_job():
 #     print('This job is run every three minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/2')
+@sched.scheduled_job('cron', minute='*/2')
 def scheduled_job():
     print('this job runs every day */2 min.')
     print(f"{datetime.datetime.now().ctime()}")
     print('====== APScheduler CRON ==========')
-    
+
     url = "https://jtimebot.heroku.com/"
     conn =urllib.request.urlopen(url)
 
     for key,value in conn.getheaders():
-        print(key,value)
+        print(key, value)
 
 sched.start()
