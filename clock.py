4e10755d28b592b345869ae1872afe8f673096e8
jtime <sgr918@gmail.com>
2022-03-16 23:29:09 +0800

    init0316_p6-15

--- clock.py
+++ clock.py
@@ -1,3 +1,4 @@
+import datetime
 import urllib.request
 
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -15,9 +16,23 @@ sched = BlockingScheduler()
 # def scheduled_job():
 #     print('this job is run every weekday at 5pm.')
 
+#0316 p6-12
+# @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
+# def scheduled_job():
+#     url = "https://jtimebot.herokuapp.com/"
+#     conn = urllib.request.urlopen(url)
+#
+#     for key, value in conn.getheaders():
+#         print(key, value)
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/2')
 def scheduled_job():
+    print("===============APScheduler CORN=========================")
+    print("===============APScheduler CORN=========================")
+    print(f"{datetime.datetime.now().ctime()}")
+    print("===============APScheduler CORN=========================")
+    print("===============APScheduler CORN=========================")
+    
     url = "https://jtimebot.herokuapp.com/"
     conn = urllib.request.urlopen(url)
 
