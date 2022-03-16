547883fb230f9360e710ef20e76b3f130c3a9921
jtime <sgr918@gmail.com>
2022-03-16 23:11:57 +0800

    init03162311

--- app/models_for_line.py
+++ app/models_for_line.py
@@ -25,31 +25,6 @@ def istock_isch(target):
 
     return img_list[random.randint(0, len(img_list) -1)]
 
-    # img_search = {'tbm': 'isch', 'q': target}
-    # query = urllib.parse.urlencode(img_search)
-    # base = "https://www.google.com/search?"
-    # url = str(base + query)
-    #
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-    #
-    # res = urllib.request.Request(url, headers=headers)
-    # con = urllib.request.urlopen(res)
-    # data = con.read()
-    #
-    # pattern = '"(https://encrypted-tbn0.gstatic.com[\S]*)"'
-    # img_list = []
-    #
-    # for match in re.finditer(pattern, str(data, "utf-8")):
-    #     if len(match.group(1)) < 150:
-    #         img_list.append(match.group(1))
-    #
-    # random_img_url = img_list[random.randint(0, len(img_list) + 1)]
-    #
-    # message = ImageSendMessage(
-    #     original_content_url=random_img_url,
-    #     preview_image_url=random_img_url
-    # )
 
 def pretty_echo(text):
 
--- clock.py
+++ clock.py
@@ -1,20 +1,28 @@
-import datetime
+
 from apscheduler.schedulers.blocking import BlockingScheduler
-import urllib.request
+
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', minute='*/2')
+# @sched.scheduled_job('cron', minute='*/2')
+# def scheduled_job():
+#     print('this job runs every day */2 min.')
+#     print(f"{datetime.datetime.now().ctime()}")
+#     print('====== APScheduler CRON ==========')
+#
+#     url = "https://jtimebot.heroku.com/"
+#     conn =urllib.request.urlopen(url)
+#
+#     for key,value in conn.getheaders():
+#         print(key, value)
+
+@sched.scheduled_job('interval', minutes=3)
+def time_job():
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
-    print('this job runs every day */2 min.')
-    print(f"{datetime.datetime.now().ctime()}")
-    print('====== APScheduler CRON ==========')
-
-    url = "https://jtimebot.heroku.com/"
-    conn =urllib.request.urlopen(url)
-
-    for key,value in conn.getheaders():
-        print(key, value)
+    print('this job is run every weekday at 5pm.')
 
 sched.start()
