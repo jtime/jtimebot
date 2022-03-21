25a8e43e57d5b43a6acd5347bd7647de9d15b92e
jtime <sgr918@gmail.com>
2022-03-21 10:48:19 +0800

    20220321 p9-15 test2

--- app/custom_models/CallDatabase.py
+++ app/custom_models/CallDatabase.py
@@ -1,8 +1,8 @@
 import os
 import psycopg2
 def insert(record_list):
-    DATABASESE_URL =os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASESE_URL, sslmode='require')
+    DATABASE_URL =os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
     table_columns = '(alpaca_name, training, duration, date)'
     postgres_insert_query = f"""INSERT INTO alpaca_training {table_columns} VALUES (%s, %s, %s, %s);"""
--- app/models_for_line_goo.py
+++ app/models_for_line_goo.py
@@ -36,8 +36,8 @@ def reply_text(event):
         if not reply:
             reply = PhoebeTalks.pretty_echo(event)
 
-        if not reply:
-            reply = goo_message(event)
+        # if not reply:
+        #     reply = goo_message(event)
 
         if not reply:
             reply = PhoebeTalks.insert_record(event)
@@ -46,43 +46,43 @@ def reply_text(event):
 
 
 
-def goo_message(event): #goo_photo
-    msg = event.message.text
-
-    try:
-        img_search = {'tbm': 'isch', 'q': msg}
-        query = urllib.parse.urlencode(img_search)
-        base = "https://www.google.com/search?"
-        url = str(base + query)
-
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
-
-        res = urllib.request.Request(url, headers=headers)
-        con = urllib.request.urlopen(res)
-        data = con.read()
-
-        pattern = '"(https://encrypted-tbn0.gstatic.com[\S]*)"'
-        img_list = []
-
-        for match in re.finditer(pattern, str(data, "utf-8")):
-            if len(match.group(1)) < 150:
-                img_list.append(match.group(1))
-
-        random_img_url = img_list[random.randint(0, len(img_list) + 1)]
-
-        message = ImageSendMessage(
-            original_content_url=random_img_url,
-            preview_image_url=random_img_url
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-        return True
-
-    except:
-        pretty_text = PhoebeTalks.pretty_echo(event.message.text)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=pretty_text)
-        )
-        return False
+# def goo_message(event): #goo_photo
+#     msg = event.message.text
+#
+#     try:
+#         img_search = {'tbm': 'isch', 'q': msg}
+#         query = urllib.parse.urlencode(img_search)
+#         base = "https://www.google.com/search?"
+#         url = str(base + query)
+#
+#         headers = {
+#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+#
+#         res = urllib.request.Request(url, headers=headers)
+#         con = urllib.request.urlopen(res)
+#         data = con.read()
+#
+#         pattern = '"(https://encrypted-tbn0.gstatic.com[\S]*)"'
+#         img_list = []
+#
+#         for match in re.finditer(pattern, str(data, "utf-8")):
+#             if len(match.group(1)) < 150:
+#                 img_list.append(match.group(1))
+#
+#         random_img_url = img_list[random.randint(0, len(img_list) + 1)]
+#
+#         message = ImageSendMessage(
+#             original_content_url=random_img_url,
+#             preview_image_url=random_img_url
+#         )
+#         line_bot_api.reply_message(event.reply_token, message)
+#         return True
+#
+#     except:
+#         pretty_text = PhoebeTalks.pretty_echo(event.message.text)
+#         line_bot_api.reply_message(
+#             event.reply_token,
+#             TextSendMessage(text=pretty_text)
+#         )
+#         return False
 
