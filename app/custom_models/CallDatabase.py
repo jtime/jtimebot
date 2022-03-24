749bafc54a14f79f4464c10f138250f768e6c961
jtime <sgr918@gmail.com>
2022-03-24 10:34:59 +0800

    202203241034 test13

--- app/custom_models/CallDatabase.py
+++ app/custom_models/CallDatabase.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 def insert(record_list):
+    print("幹林老師")
     DATABASE_URL =os.environ.get()['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
