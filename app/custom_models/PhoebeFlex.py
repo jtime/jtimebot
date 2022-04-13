f4a46d997cf26c394d6873e67f6d9f7c3a1b4b4a
jtime <sgr918@gmail.com>
2022-04-13 10:49:05 +0800

    '20220413_page10-18_test01'

---
+++ app/custom_models/PhoebeFlex.py
@@ -0,0 +1,68 @@
+def image_in_FlexMessage(url):
+    return {"type" : "image",
+            "url" : url,
+            "size" : "full",
+            "aspect_ratio" : "20:13",
+            "aspect_mode" : "cover"}
+
+def text_in_FlexMessage(text, size, color, weight="regular", wrap=False):
+    return {"type" : "text",
+            "size" : size,
+            "color" : color,
+            "weight" : weight,
+            "wrap" : wrap}
+
+def logo_in_FlexMessage(text="PhoebeFlex"):
+    return text_in_FlexMessage(text, size='md', color='#066BAF', weight='bold')
+
+def title_in_FlexMessage(text):
+    return text_in_FlexMessage(text, size='xl', color='#555555', weight='bold')
+
+def heading_in_FlexMessage(text):
+    return text_in_FlexMessage(text, size='xl', color='#555555')
+
+def note_in_FlexMessage(text):
+    return text_in_FlexMessage(text, size='md', color='#AAAAAA', wrap=True)
+
+def separator_in_FlexMessage(margin= 'xl'):
+    return {"type": "separator", "margin": margin}
+
+def button_in_FlexMessage(label, data, display_text):
+    return {"type" : "button",
+            "action" : {
+                "type" : "postback",
+                "label" : label,
+                "data" : data,
+                "display_text" : display_text
+            },
+            "style" : "link",
+            "color" : "#066BAF",
+            "height" : "sm"}
+
+def index_Flexmessage():
+    hero_image_url = "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png"
+    body_contents = [logo_in_FlexMessage(),
+                     title_in_FlexMessage("Into toe alpaca_training"),
+                     separator_in_FlexMessage(),
+                     heading_in_FlexMessage('Overall'),
+                     note_in_FlexMessage("# 查詢所有資料"),
+                     heading_in_FlexMessage("alpaca_name"),
+                     note_in_FlexMessage("# 按照 alpaca_name 查詢"),
+                     separator_in_FlexMessage()]
+
+    footer_contents = [button_in_FlexMessage('Overall', 'Overall', 'Overall'),
+                       button_in_FlexMessage('alpaca_name', 'alpaca_name','alpaca_name')]
+
+    FlexMessage = {'type': 'bubble',
+                   'hero': image_in_FlexMessage(hero_image_url),
+                   'body': {
+                       'type': 'box',
+                       'layout': 'vertical',
+                       'spacing': 'md',
+                       'contents': body_contents},
+                   'footer': {
+                       'type': 'box',
+                       'layout': 'vertical',
+                       'contents': footer_contents}
+                   }
+    return FlexMessage
--- app/custom_models/PhoebeTalks.py
+++ app/custom_models/PhoebeTalks.py
@@ -1,9 +1,20 @@
+
 from app import line_bot_api
-from app.custom_models import utils, CallDatabase
+from app.custom_models import utils, CallDatabase,PhoebeFlex
+
 
 from linebot.models import *
 import random
 
+def query_record(event):
+    if '查詢訓練紀錄' in event.message.text:
+        line_bot_api.reply_message(
+            event.reply_token,
+            FlexSendMessage(alt_text='query record: index', contents=PhoebeFlex.index_Flexmessage())
+        )
+        return True
+    else:
+        return False
 
 def pretty_echo(event):
 
